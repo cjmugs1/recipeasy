89b321118ba75f5953275238f7059a0289133b2a
coderchrisdean <coderchrisdean@gmail.com>
2023-04-07 17:02:06 -0700

    completed donationController logic for handling a donation

--- server/controllers/donationController.js
+++ server/controllers/donationController.js
@@ -1,23 +1,41 @@
 // logic to handle donations
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Donation = require("../models/Donation");
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const donationController = {
-    // create a new donation
-    async createDonation(req, res) {
-        const { amount, currency, description } = req.body;
+  // create a new donation
+  async createDonation(req, res) {
+    const { amount, currency, description } = req.body;
+    const userID = req.user?.id;
 
-        try {
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount, currency, description,
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency,
+        description,
+      });
 
-            });
+      // save donation to database
+      await Donation.create({ amount, currency, description, user: userId });
 
-            res.status(200).json({ clientSecret: paymentIntent.client_secret });
-        } catch (err) {
-            res.status(500).json({ message: 'Error processing the payment' });
-        }
-    },
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (err) {
+      res.status(500).json({ message: "Error processing the payment" });
+    }
+  },
 
-    //retrieve list of donations
-}
+  //retrieve list of donations
+
+  async getDonations(req, res) {
+    try {
+      const donations = await Donation.find().populate("user", "name");
+      res.status(200).json(donations);
+    } catch (err) {
+      res.status(500).json({ message: "Error retrieving donations." });
+    }
+  },
+};
+
+module.exports = donationController;
--- server/routes/api/donation-route.js
+++ server/routes/api/donation-route.js
@@ -1,18 +1,11 @@
 // route to handle donations
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const express = require('express'); 
 const router = express.Router();
+const donationController = require('../controller/donationController')
 
-// donation route
-
-router.post('/donate', async (req, res) => {
-    try {
-        // donation controller --> server/controllers/donationController.js
-        const donationResult = await processDonation(req.body);
-        res.status(200).json(donationResult);
-    } catch (err) {
-        res.status(500).json({ message: 'Error processing donation.', error: error.message });
-    }
-});
+// donation route - /api/donations
+router.get('/', donationController.getDonations);
+router.post('/', donationController.createDonation); 
 
 module.exports = router;
