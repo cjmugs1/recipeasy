b70a9a30d4e5270bafdb2c69cda0019b9ae1b9d9
coderchrisdean <coderchrisdean@gmail.com>
2023-04-07 17:23:34 -0700

    stripe integration

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 //set up express server with middleware and port
 //possibly serve images
 //look into translations for recipes
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -17,6 +14,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
+require('dotenv').config({ path: './.env' });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
