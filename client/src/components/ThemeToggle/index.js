15906c3b80f20d4ba9ef993be81ff7299ed407e3
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 19:51:19 -0700

    added toggleTheme component

--- client/src/components/ThemeToggle/index.js
+++ client/src/components/ThemeToggle/index.js
@@ -1,1 +1,27 @@
 // component to toggle between light and dark theme
+
+import React, { useContext } from 'react';
+import { Switch } from 'antd';
+import { ThemeContext } from '../../context/ThemeContext';
+import './themeToggle.css';
+
+const ThemeToggle = () => {
+    const { theme, setTheme } = useContext(ThemeContext);
+
+    const onThemeChange = (toggle) => {
+        setTheme(toggle ? 'dark' : 'light');
+    };
+
+    return (
+        <div className="theme-toggle">
+            <Switch
+            checked={theme === 'dark'}
+            onChange={onThemeChange}
+            className="theme-switch"
+            />
+        </div>
+    );
+};
+
+export default ThemeToggle;
+
