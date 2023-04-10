f7c9708cc46b761b6b2783f47821070971e35686
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 20:13:49 -0700

    added index.js gateway for RecipeTags

--- client/src/components/RecipeTags/index.js
+++ client/src/components/RecipeTags/index.js
@@ -1,1 +1,12 @@
 // entry way to recipetags component
+import React from 'react';
+import './tags.css';
+
+const RecipeTags = () => {
+    return (
+        <div className="recipe-tags">
+        </div>
+    );
+};
+
+export default RecipeTags;
