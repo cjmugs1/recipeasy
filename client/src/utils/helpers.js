580d64b0e3264ee9345ba3993d636ce67d58222a
Connor Mulligan <connor26m@gmail.com>
2023-04-11 20:21:52 -0600

    started on main recipe browser component, deleted files to hopefully fix breaking changes

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,13 +7,13 @@ export function pluralize(name, count) {
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open('shop-shop', 1);
+    const request = window.indexedDB.open('recipeasy', 1);
     let db, tx, store;
     request.onupgradeneeded = function(e) {
       const db = request.result;
-      db.createObjectStore('products', { keyPath: '_id' });
-      db.createObjectStore('categories', { keyPath: '_id' });
-      db.createObjectStore('cart', { keyPath: '_id' });
+      db.createObjectStore('recipes', { keyPath: '_id' });
+      // db.createObjectStore('categories', { keyPath: '_id' });
+      // db.createObjectStore('cart', { keyPath: '_id' });
     };
 
     request.onerror = function(e) {
